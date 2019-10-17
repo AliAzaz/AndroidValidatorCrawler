c960a7fe6798bbb4ead04ef1bf01cb2c0fc26e4f
ali.azaz <ali.azaz.alam@hotmail.com>
2019-10-17 18:09:11 +0500

    working on sample app activity

--- app/src/main/java/com/validator/aliazaz/MainActivity.kt
+++ app/src/main/java/com/validator/aliazaz/MainActivity.kt
@@ -2,11 +2,20 @@ package com.validator.aliazaz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.validator.aliazaz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var bi: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        bi.callback = this
+    }
+
+    fun BtnSubmit() {
+        com.validatorbox.aliazaz.Validator.emptyCheckingContainer(this, bi.formValidateLayout)
     }
 }

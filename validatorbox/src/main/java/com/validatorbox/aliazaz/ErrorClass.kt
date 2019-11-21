88cb57bb6014001de8a270a833cf4911a494e166
ali.azaz <ali.azaz.alam@hotmail.com>
2019-11-21 16:58:47 +0500

    some updation done in sub classes

--- validatorbox/src/main/java/com/validatorbox/aliazaz/ErrorClass.kt
+++ validatorbox/src/main/java/com/validatorbox/aliazaz/ErrorClass.kt
@@ -2,10 +2,8 @@ package com.validatorbox.aliazaz
 
 import android.graphics.drawable.Drawable
 
-internal class ErrorClass {
-
-    var drawable: Drawable? = null
-    var prvcolor: Int = 0
+internal data class ErrorClass(
+    var drawable: Drawable? = null,
+    var prvcolor: Int = 0,
     var id: Int = 0
-
-}
+)
--- validatorbox/src/main/java/com/validatorbox/aliazaz/ValidatorError.kt
+++ validatorbox/src/main/java/com/validatorbox/aliazaz/ValidatorError.kt
@@ -4,47 +4,41 @@ import android.content.Context
 import android.graphics.Color
 import android.view.View
 import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import com.aliazaz.validatorbox.R
 
-internal class ValidatorError {
-    companion object {
+internal object ValidatorError {
 
-        private var error: ErrorClass? = null
+    private var error: ErrorClass? = null
 
-        @JvmStatic
-        fun putError(context: Context, viewCom: View) {
+    fun putError(context: Context, viewCom: View) {
 
-            when {
-                error == null -> {
-                    error = ErrorClass()
-                    error!!.drawable = viewCom.background
-                    error!!.id = viewCom.id
-                    if (viewCom is EditText) {
-                        //Get previous color
-                        error!!.prvcolor = viewCom.currentTextColor
-                        //Set new color
-                        viewCom.setTextColor(Color.parseColor("#D8000C"))
-                    }
-
-                    viewCom.setBackgroundResource(R.drawable.image_102)
+        when (error) {
+            null -> {
+                error = ErrorClass(viewCom.background, id = viewCom.id)
+                if (viewCom is EditText) {
+                    //Get previous color
+                    error!!.prvcolor = viewCom.currentTextColor
+                    //Set new color
+                    viewCom.setTextColor(Color.parseColor("#D8000C"))
                 }
+                viewCom.background = ContextCompat.getDrawable(context, R.drawable.image_102)
             }
-
         }
 
-        @JvmStatic
-        fun clearError(viewCom: View) {
+    }
 
-            when {
-                error != null && error!!.id == viewCom.id -> {
-                    viewCom.background = error!!.drawable
-                    if (viewCom is EditText) {
-                        error!!.prvcolor.let { viewCom.setTextColor(it) }
-                    }
-                    error = null
+    fun clearError(viewCom: View) {
+
+        when {
+            error != null && error!!.id == viewCom.id -> {
+                ViewCompat.setBackground(viewCom, error!!.drawable)
+                if (viewCom is EditText) {
+                    error!!.prvcolor.let { viewCom.setTextColor(it) }
                 }
+                error = null
             }
-
         }
 
     }

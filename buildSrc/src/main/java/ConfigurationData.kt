1a473cd78a4e0cbad375fa42b12947d54eea8454
AliAzazAlam <ali.azaz.alam@hotmail.com>
2023-10-09 14:21:45 +0500

    I've worked on: * Added buildSrc for libraries management * Refactors library callings

---
+++ buildSrc/src/main/java/ConfigurationData.kt
@@ -0,0 +1,8 @@
+object ConfigurationData {
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 34
+    const val compileSdkVersion = 34
+    const val versionCode = 2
+    const val versionName = "2.0.0"
+    const val buildToolsVersion = "34.0.0"
+}

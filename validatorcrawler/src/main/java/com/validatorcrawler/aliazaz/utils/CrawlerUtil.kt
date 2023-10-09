4ede77b7aae7e4b71f5551caac082818b7dfe4a4
AliAzazAlam <ali.azaz.alam@hotmail.com>
2023-10-09 13:13:55 +0500

    I've worked on: * Updated travis * Updated README.md * Added string utils dependency. * Refactored code

--- validatorcrawler/src/main/java/com/validatorcrawler/aliazaz/utils/CrawlerUtil.kt
+++ validatorcrawler/src/main/java/com/validatorcrawler/aliazaz/utils/CrawlerUtil.kt
@@ -3,6 +3,7 @@ package com.validatorcrawler.aliazaz.utils
 import android.content.Context
 import android.content.res.Resources
 import android.view.View
+import org.apache.commons.lang3.StringUtils
 
 fun getVisibilityFlag(view: View): Boolean {
     return view.visibility == View.GONE || !view.isEnabled || (view.tag != null && view.tag == "-1")
@@ -10,18 +11,17 @@ fun getVisibilityFlag(view: View): Boolean {
 
 fun getString(context: Context, idName: String): String {
     val res = context.resources.getIdentifier(idName, "string", context.packageName)
-    return if (res != 0) context.getString(res) else ""
+    return if (res != 0) context.getString(res) else StringUtils.EMPTY
 }
 
 fun getIDComponent(view: View): String {
-    var idName: String? = ""
-    idName = try {
+    val idName: String? = try {
         view.resources.getResourceName(view.id)?.split("id/".toRegex())
             ?.dropLastWhile { it.isEmpty() }
-            ?.toTypedArray()?.get(1) + ""
+            ?.toTypedArray()?.get(1) + StringUtils.EMPTY
     } catch (_: Resources.NotFoundException) {
-        ""
+        StringUtils.EMPTY
     }
 
-    return idName ?: ""
+    return idName ?: StringUtils.EMPTY
 }
